fee55346d2b2c320192b7ca6768f7718d2662b03
Ted Kassen <tedgkassen@gmail.com>
2019-04-27 11:01:55 -0700

    DAG

---
+++ src/dag/dag.rs
@@ -0,0 +1,116 @@
+use crate::dag::node::Node;
+use crate::dag::node::Resolveable;
+
+use std::collections::HashSet;
+use std::cmp::Eq;
+use std::hash::Hash;
+use std::fmt::{Display, Formatter};
+
+#[derive(Eq, PartialEq, Debug)]
+struct Dag<'a, T>
+where T: Eq + Hash + Resolveable + Display {
+    nodes: HashSet<&'a Node<'a, T>>,
+}
+
+impl<'a, T> Dag<'a, T>
+where T: Eq + Hash + Resolveable + Display {
+    pub fn new() -> Dag<'a, T> {
+        Dag{
+            nodes: HashSet::new(),
+        }
+    }
+
+    pub fn add(&mut self, node: &'a Node<'a, T>) -> &'a mut Dag<T> {
+        self.nodes.insert(node);
+        self
+    }
+
+    pub fn dep(&'a mut self, from: &'a Node<'a, T>, to: &'a Node<'a, T>) -> &'a mut Dag<T> {
+        if self.check_cycle(from, to) {
+            panic!("Attempted edge insertion would cause cycle containing: {}. Aborting.", from)
+        }
+
+        to.add_dependant(from);
+        from.add_dependency(to);
+        self
+    }
+
+    pub fn build(&mut self) {
+    }
+
+    fn check_cycle(&mut self, start: &'a Node<'a, T>, pt: &'a Node<'a, T>) -> bool {
+        if (pt as *const Node<'a, T>) == (start as *const Node<'a, T>) {
+            true
+        } else if pt.dependencies.borrow().is_empty() {
+            false
+        } else {
+            pt.dependencies.borrow()
+                .iter()
+                .any(|&dep| self.check_cycle(start, dep))
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::dag::node::Resolveable;
+
+    #[derive(Hash, Eq, PartialEq, Debug)]
+    struct MockResolveable {
+        id: char,
+        done: bool
+    }
+
+    impl MockResolveable {
+        fn new(id: char) -> MockResolveable {
+            MockResolveable{id, done: false}
+        }
+    }
+
+    impl Display for MockResolveable {
+        fn fmt(&self, f: &mut Formatter) -> std::fmt::Result {
+            write!(f, "{}", self.id)
+        }
+    }
+
+    impl Resolveable for MockResolveable {
+        fn start(&mut self) { self.done = true }
+        fn is_started(&self) -> bool { self.done }
+        fn is_resolved(&self) -> bool { self.done }
+    }
+
+    #[test]
+    #[should_panic]
+    fn build_dag() {
+        let a = Node::new(MockResolveable::new('A'));
+        let b = Node::new(MockResolveable::new('B'));
+        let c = Node::new(MockResolveable::new('C'));
+        let d = Node::new(MockResolveable::new('D'));
+        let e = Node::new(MockResolveable::new('E'));
+        let f = Node::new(MockResolveable::new('F'));
+        let g = Node::new(MockResolveable::new('G'));
+        let h = Node::new(MockResolveable::new('H'));
+        
+        let mut dag = Dag::new();
+
+        dag.add(&a)
+            .add(&b)
+            .add(&c)
+            .add(&d)
+            .add(&e)
+            .add(&f)
+            .add(&g)
+            .add(&h)
+            .dep(&b, &a)
+            .dep(&c, &b)
+            .dep(&d, &b)
+            .dep(&d, &c)
+            .dep(&e, &d)
+            .dep(&f, &d)
+            .dep(&g, &f)
+            .dep(&h, &f)
+            .dep(&c, &f)
+            .build();
+    }
+}
---
+++ src/dag/node.rs
@@ -0,0 +1,55 @@
+use std::collections::HashSet;
+use std::hash::{Hash, Hasher};
+use std::cell::RefCell;
+use std::fmt::{Display, Formatter, Result};
+
+pub trait Resolveable {
+    fn start(&mut self) -> ();
+    fn is_resolved(&self) -> bool;
+    fn is_started(&self) -> bool;
+}
+
+#[derive(Eq, PartialEq, Debug)]
+pub struct Node<'a, T>
+where T: Eq + Resolveable + Hash + Display {
+    pub value: T,
+    pub dependants: RefCell<HashSet<&'a Node<'a, T>>>,
+    pub dependencies: RefCell<HashSet<&'a Node<'a, T>>>
+}
+
+impl<'a, T> Display for Node<'a, T>
+where T: Eq + Resolveable + Hash + Display {
+    fn fmt(&self, f: &mut Formatter) -> Result {
+        write!(f, "(Node: {})", self.value)
+    }
+}
+
+impl<'a, T> Hash for Node<'a, T>
+where T: Eq + Resolveable + Hash + Display {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.value.hash(state);
+    }
+}
+
+impl<'a, T> Node<'a, T>
+where T: Eq + Resolveable + Hash + Display {
+    pub fn new(v: T) -> Node<'a, T> {
+        Node{
+            value: v,
+            dependants: RefCell::new(HashSet::new()),
+            dependencies: RefCell::new(HashSet::new())
+        }
+    }
+
+    pub fn add_dependency(&self, dep: &'a Node<'a, T>) {
+        self.dependencies.borrow_mut().insert(dep);
+    }
+
+    pub fn add_dependant(&self, dep: &'a Node<'a, T>) {
+        self.dependants.borrow_mut().insert(dep);
+    }
+
+    pub fn resolved(&self) -> bool {
+        self.value.is_resolved()
+    }
+}
